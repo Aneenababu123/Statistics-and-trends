99d82a9934f7aad118fa08816d04389b74cc2bca
Aneenababu123 <126000626+Aneenababu123@users.noreply.github.com>
2023-04-05 01:26:01 +0100

    Add files via upload
    Added bar plots code

--- Trends.py
+++ Trends.py
@@ -46,3 +46,44 @@ def filter_data(df, col, value, con, yr):
     da2 = da2.transpose()
     #returning the dataframe for the heat map
     return da1, da2
+
+def plot1(data, title, x, y):
+    """Function for bar plot"""
+    
+    #Reading data the from for dataframe
+    ax = data.plot.bar(x='Country Name', rot=0, figsize=(50, 30), fontsize=50)
+    #Sets the location for y axis
+    ax.set_yticks([0, 20, 40, 60, 80])
+    #Set the title for plot
+    ax.set_title(title, fontsize=50)
+    #Set label for x axis
+    ax.set_xlabel(x, fontsize=50)
+    #Set label for y label
+    ax.set_ylabel(y, fontsize=50)
+    ax.legend(fontsize=50)
+    #Save the bar plot as png
+    plt.savefig(title + '.png')
+    plt.show()
+    return
+
+#Country1 used for the data analysis
+country1 = ['India', 'Pakistan', 'Peru', 'Afghanistan']
+#years used for the data analysis
+year = ['2000', '2003', '2004', '2006', '2008', '2010']
+#Reads data from csv file
+data = read_data("Environment.csv")
+da2, da3 = filter_data(
+    data, 'Indicator Name', 'CO2 emissions from solid fuel consumption (% of total)', country1, year)
+#Print the data
+print(da2)
+print(da3)
+#Calling the bar plot function.
+plot1(da2, 'C02 emissions', 'Countries', 'C02 emissions')
+
+da4, da5 = filter_data(
+    data, 'Indicator Name', 'CO2 emissions from liquid fuel consumption (% of total)', country1, year)
+print(da4)
+print(da5)
+#Calling the bar plot function.
+plot1(da4, 'CO2 Emissions', 'Countries', 'C02 Emissions')
+
